d978e1341de223c6a2774e78a28d7e0f74cb78f8
Tom <T.Langwaldt@gmail.com>
2016-07-12 19:59:35 +0200

    Weird Eclipse Git UI error

---
+++ Towers/src/AutoplaySolve.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+public class AutoplaySolve extends Thread{
+
+	GameController gameController;
+	boolean isMoving;
+	
+	
+	
+	public AutoplaySolve(GameController gameController, boolean isMoving)
+	{
+		this.gameController = gameController;
+		this.isMoving = isMoving;
+		this.start();
+		
+	}
+	
+	@Override
+	public void run() {
+		while(isMoving)
+		{
+			Autoplay(gameController.getSettings().getTotalDisks(),gameController.getRods().get(0),gameController.getRods().get(1),gameController.getRods().get(2));
+		}
+			
+		
+	}
+	public void Autoplay(int diskCount,Stack<Disk> rod1,Stack<Disk> rod2,Stack<Disk> rod3)
+	{
+		if(diskCount == 1)
+		{
+			gameController.moveDisks(rod1, rod3);
+			gameController.refresh();
+			try {
+				this.sleep(gameController.getSettings().getStepDelay());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else
+		{
+			Autoplay(diskCount-1, rod1,rod3,rod2);
+			gameController.moveDisks(rod1, rod2);
+			gameController.refresh();
+			
+			Autoplay(diskCount-1,rod2,rod1,rod3);
+		}
+	}
+}
